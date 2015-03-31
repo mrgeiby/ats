9a78b719b23cd573b2e587b5b5b7585a7866a1e7
mrgeiby <mike@mrgeiby.co.uk>
2015-03-31 23:01:09 +0100

    Update registrar service

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -4,17 +4,18 @@ use App\User;
 use Validator;
 use Illuminate\Contracts\Auth\Registrar as RegistrarContract;
 
-class Registrar implements RegistrarContract {
+class Registrar implements RegistrarContract
+{
 
-	/**
-	 * Get a validator for an incoming registration request.
-	 *
-	 * @param  array  $data
-	 * @return \Illuminate\Contracts\Validation\Validator
-	 */
-	public function validator(array $data)
-	{
-		return Validator::make($data, [
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    public function validator(array $data)
+    {
+        return Validator::make($data, [
             'firstName' => 'required|max:100',
             'lastName' => 'required|max:100',
             'address' => 'required|max:255',
@@ -22,22 +23,22 @@ class Registrar implements RegistrarContract {
             'postCode' => 'required|min:3|max:9',
             'dateOfBirth' => 'required|date',
             'phoneNumber' => 'required|min:11|max:13',
-			'email' => 'required|email|max:255|unique:users',
-			'password' => 'required|confirmed|min:6',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:6',
             'role' => 'required',
-		]);
-	}
+        ]);
+    }
 
-	/**
-	 * Create a new user instance after a valid registration.
-	 *
-	 * @param  array  $data
-	 * @return User
-	 */
-	public function create(array $data)
-	{
-		return User::create([
-			'firstName' => $data['firstName'],
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array $data
+     * @return User
+     */
+    public function create(array $data)
+    {
+        $user = User::create([
+            'firstName' => $data['firstName'],
             'lastName' => $data['lastName'],
             'address' => $data['address'],
             'town' => $data['town'],
@@ -45,9 +46,17 @@ class Registrar implements RegistrarContract {
             'dob' => $data['dateOfBirth'],
             'phoneNumber' => $data['phoneNumber'],
             'email' => $data['email'],
-			'password' => bcrypt($data['password']),
+            'password' => bcrypt($data['password']),
             'role_id' => $data['role'],
-		]);
-	}
+        ]);
+
+        if ($data['role'] == 3) {
+
+        } else if ($data['role'] == 4) {
+
+        }
+
+        return $user;
+    }
 
 }
