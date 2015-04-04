341ec5cc82fd5f0de9cb1a946b32167bbeaeb968
mrgeiby <mike@mrgeiby.co.uk>
2015-04-04 19:27:15 +0100

    Update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,11 +15,13 @@ Route::get('/', 'HomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-Route::group(array('prefix' => 'user', 'middleware' => ['auth', 'roles'], 'roles' => ['consumer', 'engineer']), function () {
-    Route::get('show', 'UserController@show');
-    //Route::get('edit', 'UserController@edit');
-    //Route::post('update', 'UserController@update');
-});
+Route::get('user/show', 'UserController@show');
+
+//Route::group(array('prefix' => 'user', 'middleware' => ['auth', 'roles'], 'roles' => ['consumer', 'engineer']), function () {
+//    Route::get('show', 'UserController@show');
+//    //Route::get('edit', 'UserController@edit');
+//    //Route::post('update', 'UserController@update');
+//});
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
