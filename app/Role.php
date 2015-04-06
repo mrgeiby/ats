cc4515f4b5aab98f462e1785abf93af4f97c723d
mrgeiby <mike@mrgeiby.co.uk>
2015-04-06 17:06:58 +0100

    Update Role Model

--- app/Role.php
+++ app/Role.php
@@ -6,6 +6,8 @@ class Role extends Model {
 
     protected $table = 'roles';
 
+    protected $fillable = ['name', 'description'];
+
     public function users()
     {
         return $this->hasMany('App\User', 'role_id', 'id');
