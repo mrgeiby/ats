69683e4a82e6227607fbcb38fc9ce26fa19a0ae3
mrgeiby <mike@mrgeiby.co.uk>
2015-04-08 15:54:02 +0100

    Add link on homepage main navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,6 +36,7 @@
             <ul class="nav navbar-nav">
                 <li><a href="{{ url('/') }}">Home</a></li>
                 <li><a href="{{ url('/') }}">About Us</a></li>
+                <li><a href="{{ url('/') }}">Engineer Search</a></li>
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
